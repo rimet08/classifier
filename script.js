d3df714f1d55506086a92f01143155713236ce29
rimet08 <rmetzner@proton.me>
2025-11-02 10:12:13 +0100

    3rd and 4th if

--- script.js
+++ script.js
@@ -13,7 +13,19 @@ else {
     // default image
 }
 
-if (ageInput = 0 && 6 ) {
+if (ageInput >= 0 && ageInput <= 6 ) {
     let genderMale = "images/dite_muz.png"
-    let genderFemale = "images/dite_zen.png"
+    let genderFemale = "images/dite_zena.png"
 }
+
+if (ageInput > 7 && ageInput < 15 ) {
+    let genderMale = "images/skolak_muz.png"
+    let genderFemale = "images/skolak_zena.png"
+}
+
+if (ageInput >= 16 && ageInput < 26 ) {
+    let genderMale = "images/student_muz.png"
+    let genderFemale = "images/student_zena.png"
+}
+
+if
