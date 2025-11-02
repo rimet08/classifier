d692e278ff9c02fd9547ce619d7b1729efec3e8e
rimet08 <rmetzner@proton.me>
2025-11-02 10:15:05 +0100

    nameInput

--- script.js
+++ script.js
@@ -14,26 +14,31 @@ else {
 }
 
 if (ageInput >= 0 && ageInput <= 6 ) {
+    let nameInput = nameInput.value;
     let genderMale = "images/dite_muz.png"
     let genderFemale = "images/dite_zena.png"
 }
 
 if (ageInput > 7 && ageInput < 15 ) {
+    let nameInput = nameInput.value;
     let genderMale = "images/skolak_muz.png"
     let genderFemale = "images/skolak_zena.png"
 }
 
 if (ageInput >= 16 && ageInput < 26 ) {
+    let nameInput = nameInput.value;
     let genderMale = "images/student_muz.png"
     let genderFemale = "images/student_zena.png"
 }
 
 if (ageInput >= 27 && ageInput < 64 ) {
+    let nameInput = nameInput.value;
     let genderMale = "images/dospely_muz.png"
     let genderFemale = "images/dospela_zena.png"
 }
 
 if (ageInput >= 65 ) {
+    let nameInput = nameInput.value;
     let genderMale = "images/duchodce_muz.png"
     let genderFemale = "images/duchodce_zena.png"
 }
