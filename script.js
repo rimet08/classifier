e77ed39970b507d3d2fbceecb476bf0e444605ac
rimet08 <rmetzner@proton.me>
2025-11-02 10:01:18 +0100

    First if

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ const genderFemale = document.getElementById("genderFemale");
 const processBtn = document.getElementById("processBtn");
 const resultImage = document.getElementById("resultImage");
 
+if (ageInput) < 0 {
+    alert("Enter a valid age");
+}
+else {
+    // default image
+}
+
