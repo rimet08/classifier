02161658f38938e9aa684070cf999ea1730d0aa2
rimet08 <rmetzner@proton.me>
2025-11-02 09:58:03 +0100

    const

---
+++ script.js
@@ -0,0 +1,8 @@
+const nameInput = document.getElementById("nameInput");
+const ageInput = document.getElementById("ageInput");
+const genderMale = document.getElementById("genderMale");
+const genderFemale = document.getElementById("genderFemale");
+
+const processBtn = document.getElementById("processBtn");
+const resultImage = document.getElementById("resultImage");
+
